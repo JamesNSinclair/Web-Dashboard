808b571626701acd2ddc50e2bcafee7a062077e0
JamesNSinclair <53437429+JamesNSinclair@users.noreply.github.com>
2020-05-07 15:44:00 +0100

    Final FINAL Version :)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const alertBanner = document.getElementById('alert');
 const notifications = document.getElementById('notifications');
+const userField = document.getElementById('userField');
+const messageField = document.getElementById('messageField');
+const send = document.getElementById('send');
 
 alertBanner.innerHTML =
     `<div class="alert-banner">
@@ -13,4 +16,20 @@ alertBanner.addEventListener('click', e => {
         alertBanner.style.display = 'none',
         notifications.style.display = 'none'
     }
+});
+
+
+send.addEventListener('click', () => {
+      event.preventDefault();
+
+      if (userField.value === "") {
+        alert("Looks like you forgot to select a use, you fool of a took! Please try again :)");
+    } else if (messageField.value === "") {
+        alert("Looks like you forgot to write a message, you silly-billy! Please try again :)");
+    } else {
+        alert("That's the best message I have seen this page loaded. Kudos!");
+        document.getElementById("messageField").value = "";
+        document.getElementById("userField").value = "";
+    }
+
 });
